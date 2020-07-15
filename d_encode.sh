6d81a54373a35d0b729f0f207d989c1c5b2abb0d
zhenxiongchen <zhenxiongchen@deepglint.com>
2020-07-15 16:32:39 +0800

    fix:d_encode.sh

--- d_encode.sh
+++ d_encode.sh
@@ -1,1 +1,2 @@
-ffmpeg -f image2 -i ./d2to1_png/%d.png  -vcodec libx264 -r 25  d1.h264
+#ffmpeg -f image2 -i ./d2to1_png/%d.png -keyint_min 0 -g 10 -preset veryslow -qp 0 -vcodec libx264 -r 25  d1.h264
+ffmpeg -f image2 -i ./d2to1_png/%d.png -keyint_min 0 -g 10  -vcodec libx264 -r 25  d1.h264
